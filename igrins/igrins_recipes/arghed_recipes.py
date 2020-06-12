2be4b3a139bd3f442b87254a0e3e02ffe6614ce3
Nathan Miller <Nathan.J.Miller@nasa.gov>
2020-06-12 09:38:06 -0400

    Adding in register-thar and wvlsol-thar options.

--- igrins/igrins_recipes/arghed_recipes.py
+++ igrins/igrins_recipes/arghed_recipes.py
@@ -39,6 +39,8 @@ def get_recipe_list():
                    create_argh_command("combine-sky", ["SKY", "SKY_AB"]),
                    create_argh_command("register-sky", ["SKY", "SKY_AB"]),
                    create_argh_command("wvlsol-sky", ["SKY", "SKY_AB"]),
+                   create_argh_command("register-thar", ["THAR"]),
+                   create_argh_command("wvlsol-thar", ["THAR"]),
                    create_argh_command("extract-sky", ["SKY", "SKY_AB"]),
                    create_argh_command("extract-arc", ["ARC_*"]),
                    create_argh_command("a0v-ab", ["A0V_AB"]),
