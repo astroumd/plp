01c97b38cce7994f78eb8201346d35c862bdfa72
Nathan Miller <Nathan.J.Miller@nasa.gov>
2020-07-02 14:28:27 -0400

    Code to generate web interface to output spectra

--- igrins/igrins_recipes/recipe_plot_spec.py
+++ igrins/igrins_recipes/recipe_plot_spec.py
@@ -6,13 +6,10 @@ import pandas as pd
 from ..pipeline.steps import Step, ArghFactoryWithShort
 from ..igrins_libs.logger import logger
 
-# from ..procedures.sky_spec import make_combined_image_sky
-
-# from ..utils.image_combine import image_median
-
 from ..igrins_libs.a0v_obsid import get_group2, get_a0v_obsid
 from ..igrins_libs.igrins_config import IGRINSConfig
 from ..igrins_libs.oned_spec_helper import OnedSpecHelper
+from ..igrins_libs.path_info import ensure_dir, get_zeropadded_groupname
 from ..igrins_libs.resource_helper_igrins import ResourceHelper
 
 
@@ -175,24 +172,43 @@ def _save_to_pngs(fig_list, path, mastername):
     #Function is copied from old code
     figlist_to_pngs(figout, fig_list)
 
+def get_fixed_i1i2_list(order_indices, i1i2_list):
+    i1i2_list2 = []
+    for o_index in order_indices:
 
-def _save_to_html(path, mastername):
-    i1i2_list = get_i1i2_list(extractor,
-                              orders_w_solutions)
+        i1i2_list2.append(i1i2_list[o_index])
+    return i1i2_list2
 
+def get_i1i2_list(orders_w_solutions, new_orders, i1i2_list_):
+    
+    order_indices = []
+
+    for o in orders_w_solutions:
+        o_new_ind = np.searchsorted(new_orders, o)
+        order_indices.append(o_new_ind)
+
+    i1i2_list = get_fixed_i1i2_list(order_indices, i1i2_list_)
+
+    return i1i2_list
+
+def _save_to_html(config, groupname, tgt, utdate, band, orders_w_solutions,
+                  i1i2_list, basename_postfix=None,
+                  a0v=None, tgt_spec_cor=None):
+    
+    basename_postfix = None if basename_postfix == '' else basename_postfix
     if basename_postfix is not None:
         igr_log.warn("For now, no html output is generated if basename-postfix option is used")
     else:
         dirname = config.get_value('HTML_PATH', utdate)
-        from ..libs.path_info import get_zeropadded_groupname
 
         objroot = get_zeropadded_groupname(groupname)
         html_save(utdate, dirname, objroot, band,
                   orders_w_solutions, tgt.um,
                   tgt.spec, tgt.sn, i1i2_list)
 
-        if FIX_TELLURIC:
+        if a0v is not None:
             objroot = get_zeropadded_groupname(groupname)+"A0V"
+
             html_save(utdate, dirname, objroot, band,
                       orders_w_solutions, tgt.um,
                       a0v.flattened, tgt_spec_cor, i1i2_list,
@@ -201,12 +217,22 @@ def _save_to_html(path, mastername):
 
 def plot_spec(obsset, interactive=False,
               multiply_model_a0v=False,
-              html_output=False):
+              html_output=False,
+              threshold_a0v=0.2):
     obsdate = obsset.rs.basename_helper.obsdate
+    band = obsset.rs.basename_helper.band
+
     config = IGRINSConfig(expt=obsset.expt)
     recipe = obsset.recipe_name
     target_type, nodding_type = recipe.split("_")
     master_obsid = obsset.master_obsid
+    groupname = obsset.groupname
+
+    helper = ResourceHelper(obsset)
+    orders_w_solutions = helper.get("orders")
+
+    a0v = None
+    tgt_spec_cor = None
 
     if target_type in ["A0V"]:
         FIX_TELLURIC = False
@@ -237,6 +263,24 @@ def plot_spec(obsset, interactive=False,
 
         _plot_div_a0v_spec(fig1, tgt, obsset,
                            multiply_model_a0v=multiply_model_a0v)
+            
+        if html_output:
+   
+            a0v_obsid = None
+            a0v_obsid = get_a0v_obsid(obsset, a0v, a0v_obsid)
+            if a0v_obsid is None:
+                a0v_obsid_ = obsset.query_resource_basename("a0v")
+                a0v_obsid = obsset.rs.parse_basename(a0v_obsid_)
+            a0v_obsset = type(obsset)(obsset.rs, "A0V_AB", [a0v_obsid], ["A"],
+                                      basename_postfix="")
+
+            a0v = OnedSpecHelper(a0v_obsset)
+
+            tgt_spec_cor = get_tgt_spec_cor(obsset, tgt, a0v,
+                                            threshold_a0v,
+                                            multiply_model_a0v)
+            
+            
 
     if fig_list:
         for fig in fig_list:
@@ -249,7 +293,16 @@ def plot_spec(obsset, interactive=False,
             _save_to_pngs(fig_list, path, mastername)
     
     if html_output:
-        _save_to_html(path, mastername)
+        prod = obsset.load_resource_for("order_flat_json")
+
+        new_orders = prod["orders"]
+        i1i2_list_ = prod["i1i2_list"]
+
+        i1i2_list = get_i1i2_list(orders_w_solutions, new_orders, i1i2_list_)
+        
+        _save_to_html(config, groupname, tgt, obsdate, band, orders_w_solutions,
+                      i1i2_list, basename_postfix=obsset.basename_postfix,
+                      a0v=a0v, tgt_spec_cor=tgt_spec_cor)
 
     if do_interactive_figure:
         import matplotlib.pyplot as plt
@@ -288,6 +341,77 @@ def html_save(utdate, dirname, objroot, band,
                                   spec_js_name=spec_js_name)
         htmlname = master_root + ".html"
         open(os.path.join(dirname, htmlname), "w").write(ss)
+
+def save_for_html(dir, name, band, orders, wvl_sol, s_list1, s_list2):
+
+    ensure_dir(dir)
+
+    # Pandas requires the byte order of data (from fits) needs to be
+    # converted to native byte order of the computer this script is
+    # running.
+    wvl_sol = [w.byteswap().newbyteorder() for w in wvl_sol]
+    s_list1 = [s1.byteswap().newbyteorder() for s1 in s_list1]
+    s_list2 = [s2.byteswap().newbyteorder() for s2 in s_list2]
+
+    df_even_odd = {}
+    for o, wvl, s in zip(orders, wvl_sol, s_list1):
+        if len(wvl) < 2: continue
+        oo = ["even", "odd"][o % 2]
+        dn = 'order_%s'%oo
+        df = pd.DataFrame({dn: s},
+                          index=wvl)
+        df[dn][wvl[0]] = "NaN"
+        df[dn][wvl[-1]] = "NaN"
+
+        df_even_odd.setdefault(oo, []).append(df)
+
+    df_list = [pd.concat(v).fillna("NaN") for v in df_even_odd.values()]
+    df1 = df_list[0].join(df_list[1:], how="outer")
+
+    #df_list = []
+    df_even_odd = {}
+    for o, wvl, s in zip(orders, wvl_sol, s_list2):
+        if len(wvl) < 2: continue
+        oo = ["even", "odd"][o % 2]
+        dn = 'order_%s'%oo
+        df = pd.DataFrame({dn: s},
+                          index=wvl)
+
+        df[dn][wvl[0]] = "NaN"
+        df[dn][wvl[-1]] = "NaN"
+
+        df_even_odd.setdefault(oo, []).append(df)
+
+        #df_list.append(df)
+    df_list = [pd.concat(v).fillna("NaN") for v in df_even_odd.values()]
+    df2 = df_list[0].join(df_list[1:], how="outer")
+
+    igrins_spec_output1 = "igrins_spec_%s_%s_fig1.csv.html" % (name, band)
+    igrins_spec_output2 = "igrins_spec_%s_%s_fig2.csv.html" % (name, band)
+
+
+    df1.to_csv(os.path.join(dir, igrins_spec_output1))
+    df2.to_csv(os.path.join(dir, igrins_spec_output2))
+
+    wvlminmax_list = []
+    filtered_orders = []
+    for o, wvl in zip(orders, wvl_sol):
+        if len(wvl) > 2:
+            filtered_orders.append(o)
+            wvlminmax_list.append([min(wvl), max(wvl)])
+
+    f = open(os.path.join(dir, "igrins_spec_%s_%s.js"%(name, band)),"w")
+    f.write('name="%s : %s";\n' % (name,band))
+    f.write("wvl_ranges=")
+    f.write(str(wvlminmax_list))
+    f.write(";\n")
+    f.write("order_minmax=[%d,%d];\n" % (filtered_orders[0],
+                                         filtered_orders[-1]))
+
+    f.write('first_filename = "%s";\n' % igrins_spec_output1)
+    f.write('second_filename = "%s";\n' % igrins_spec_output2)
+
+    f.close()
 
 steps = [Step("Set basename_postfix", set_basename_postfix,
               basename_postfix=''),
---
+++ igrins/igrins_recipes/recipe_publish_html.py
@@ -0,0 +1,72 @@
+import os
+
+from jinja2 import Environment, FileSystemLoader
+
+from ..igrins_libs.igrins_config import IGRINSConfig
+from ..igrins_libs.path_info import get_zeropadded_groupname
+from ..pipeline.steps import Step
+from ..utils.json_helper import json_dumps
+
+sources = []
+
+def publish_html(obsset):
+    re = obsset.recipe_entry
+
+    utdate = obsset.rs.basename_helper.obsdate
+    band = obsset.rs.basename_helper.band
+
+    config = IGRINSConfig()
+
+    dirname = config.get_value("HTML_PATH", utdate)
+
+    env = Environment(loader=FileSystemLoader('jinja_templates'))
+    template = env.get_template('index.html')
+
+    #sources = make_html(utdate, dirname)
+
+    #Look is sources array to see if the other band has already been generated
+    tmp = ' '
+    obsids = tmp.join(re["obsids"])
+    nsources = len(sources)
+    has_source = False
+    for i in range(nsources):
+        if obsids == sources[i]["obsids"]:
+            source = sources[i]
+            has_source = True
+            break
+
+    if not has_source:
+        source = {"name": re["objname"],
+                  "obj": re["obstype"],
+                  "grp1": re["group1"],
+                  "grp2": re["group2"],
+                  "exptime": float(re["exptime"]),
+                  "recipe": re["recipe"],
+                  "obsids": tmp.join(re["obsids"]),
+                  "frametypes": tmp.join(re["frametypes"]),
+                  "nexp": len(re["obsids"]),
+                  }
+
+        sources.append(source)
+
+    objroot = get_zeropadded_groupname(source["grp1"])
+    
+    p = "igrins_spec_%s_%s.html" % (objroot, band)
+    if os.path.exists(os.path.join(dirname, p)):
+        source["url_%s" % band] = p
+
+    p = "igrins_spec_%sA0V_%s.html" % (objroot, band)
+    if os.path.exists(os.path.join(dirname, p)):
+        source["url_%s_A0V" % band] = p
+            
+
+    json_dumps(dict(utdate=utdate,
+                    sources=[source]),
+               open(os.path.join(dirname, "summary.json"), "w"))
+
+    #This will only render the obsset sent to this function
+    s = template.render(utdate=utdate, sources=sources)
+    open(os.path.join(dirname, "index.html"), "w").write(s)
+
+steps = [Step("Publish HTML", publish_html),
+        ]
